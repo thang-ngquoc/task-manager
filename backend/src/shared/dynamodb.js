77d212869baba23bc7512b087b2e2dc52f4d3926
Bao Nguyen <124044476+tbnguyen274@users.noreply.github.com>
2026-06-04 00:41:11 +0700

    fix(be): fix dynamodb to be compatible for both local and prod

--- backend/src/shared/dynamodb.js
+++ backend/src/shared/dynamodb.js
@@ -3,18 +3,25 @@ require("dotenv").config();
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
-const client = new DynamoDBClient({
-    region: process.env.AWS_REGION,
-    endpoint: process.env.DYNAMODB_ENDPOINT,
-    credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    }
-});
+const config = {
+  region: process.env.AWS_REGION || "ap-southeast-1",
+};
 
+if (process.env.DYNAMODB_ENDPOINT) {
+  config.endpoint = process.env.DYNAMODB_ENDPOINT;
+}
+
+if (process.env.ACCESS_KEY_ID && process.env.SECRET_ACCESS_KEY) {
+  config.credentials = {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  };
+}
+
+const client = new DynamoDBClient(config);
 const docClient = DynamoDBDocumentClient.from(client);
 
 module.exports = {
-   docClient,
-   client
+  docClient,
+  client,
 };
